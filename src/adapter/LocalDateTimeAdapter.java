1e9293a71524dd62f2ec45fa0812ea9008baa435
Rinat Mustafin <rin.mustafin2010@yandex.ru>
2025-03-11 06:30:51 +0500

    Добавить веб-сервер и добавить переменную

--- src/adapter/LocalDateTimeAdapter.java
+++ src/adapter/LocalDateTimeAdapter.java
@@ -14,6 +14,7 @@ import java.time.format.DateTimeFormatter;
 public class LocalDateTimeAdapter extends TypeAdapter<LocalDateTime> {
 
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
     @Override
     public void write(JsonWriter jsonWriter, LocalDateTime localDateTime) throws IOException {
         jsonWriter.value(localDateTime.format(formatter));
